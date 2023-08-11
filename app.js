3287d0e9d28e85ea81b5fa8d7ad1449444608369
Marat Mustafin <mustafin.m-marat@yandex.ru>
2023-08-11 04:02:30 +0400

    sprint 14 check test2

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-const helmet = require("helmet");
-const routesUsers = require("./routes/users");
-const routesCards = require("./routes/cards");
+const helmet = require('helmet');
+const routesUsers = require('./routes/users');
+const routesCards = require('./routes/cards');
 
-const auth = require("./middlewares/auth");
+const auth = require('./middlewares/auth');
 
 // const BadRequestError = require("./errors/bad-request-error");
 // const ExistsDatabaseError = require("./errors/exists-database-error");
@@ -14,8 +14,7 @@ const auth = require("./middlewares/auth");
 // const ServerError = require("./errors/server-error");
 // const UnauthorizedError = require("./errors/unauthorized-error");
 
-const { PORT = 3000, DB_URL = "mongodb://127.0.0.1:27017/mestodb" } =
-  process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const NOT_FOUND_CODE = 404;
 
@@ -32,17 +31,17 @@ app.use(helmet());
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/", routesUsers);
+app.use('/', routesUsers);
 
-app.patch("*", (req, res) => {
+app.patch('*', (req, res) => {
   res.status(NOT_FOUND_CODE).send({
-    message: "Несуществующий адрес сайта",
+    message: 'Несуществующий адрес сайта',
   });
 });
 
 // app.use(auth);
 
-app.use("/", auth, routesCards);
+app.use('/', auth, routesCards);
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
@@ -52,27 +51,29 @@ app.use((err, req, res, next) => {
   //   return;
   // }
 
-  if (err.kind === "ObjectId") {
+  if (err.kind === 'ObjectId') {
     res.status(400).send({
-      message: "Некорректный запрос",
+      message: 'Некорректный запрос',
     });
     return;
   }
-  if (err.name === "ValidationError") {
+  if (err.name === 'ValidationError') {
     res.status(400).send({
-      message: "Переданы некорректные данные",
+      message: 'Переданы некорректные данные',
     });
     return;
   }
   if (err.code === 11000) {
     res.status(409).send({
-      message: "Уже существует такой пользователь",
+      message: 'Уже существует такой пользователь',
     });
     return;
   }
   res.status(statusCode).send({
-    message: statusCode === 500 ? "На сервере произошла ошибка" : message,
+    message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
   });
+
+  next();
 });
 
 app.listen(PORT, () => {
